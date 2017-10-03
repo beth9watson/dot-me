9cbf38f6b85fb2ceb7d5c3e21b96bc9a7eb4db88
Bethany Watson <bwatson2@binghamton.edu>
2017-10-03 08:06:22 -0400

    Use all caps like a civilized person.

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,13 +1,13 @@
 module ContactHelper
   def github_url
-    ENV['github_url']
+    ENV['GITHUB_URL']
   end
 
   def gmail_addr(sendto = true)
-    "#{'mailto:' if sendto}#{ENV['gmail_addr']}"
+    "#{'mailto:' if sendto}#{ENV['GMAIL_ADDR']}"
   end
 
   def linkedin_url
-    ENV['linkedin_url']
+    ENV['LINKEDIN_URL']
   end
 end
