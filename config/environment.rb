ac14bf0d4e4877efb610f6f7259efb9d796e77c8
Bethany Watson <bwatson2@binghamton.edu>
2017-10-03 08:01:58 -0400

    Use config vars for contact info.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load local configuration: 
+# http://tammersaleh.com/posts/managing-heroku-environment-variables-for-local-development/
+require File.join(File.dirname(__FILE__), 'boot')
+local_env = File.join(Rails.root, 'config', 'local_env.rb')
+load(local_env) if File.exists?(local_env)
+
 # Initialize the Rails application.
 Rails.application.initialize!
