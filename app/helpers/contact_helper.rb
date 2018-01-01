4a5890dcf16c651f653d283ffc7696b68a92afd4
Bethany Watson <bwatson2@binghamton.edu>
2018-01-01 17:59:02 -0500

    Add mac demo for racko.

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -3,6 +3,10 @@ module ContactHelper
     "<a href=#{resume_url} download> Download PDF </a>".html_safe
   end
 
+  def download_racko_demo_mac
+    "<a href='images/ruby-racko-0.1.0-osx.tar.gz'} download> Download Demo for Mac </a>".html_safe
+  end
+
   def github_url
     ENV['GITHUB_URL']
   end
