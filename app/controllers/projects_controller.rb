8fe3b46294007807f09b7feaa9dd192adfefd87c
Bethany Watson <bwatson2@binghamton.edu>
2018-01-01 14:07:03 -0500

    Redo projects, add racko.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,11 @@
 class ProjectsController < ApplicationController
   def show
-    @projects = projects
+    gh_client = GithubClientService.new
+    @racko = gh_client.get_repo(ENV['GITHUB_REPO_RACKO'])
+    @carshare = gh_client.get_repo(ENV['GITHUB_REPO_CARSHARE'])
   end
 
   def carshare_estimator
     render 'projects/carshare/carshare_estimator'
-  end
-
-  private
-
-  def projects
-    gh_client = GithubClientService.new
-    ENV['GITHUB_FEATURED_REPOS'].split(",").sort.map do |repo_name|
-      gh_client.get_repo(repo_name)
-    end
   end
 end
